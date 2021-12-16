8688c5c984c482f44bc1c26f5d4c984f4ec1e397
Simon Pirko <simonpirko@ridelex.io>
2021-12-16 20:48:04 +0300

    first commit

---
+++ src/main/java/by/fakeonliner/configuration/WebConfiguration.java
@@ -0,0 +1,9 @@
+package by.fakeonliner.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("by.fakeonliner")
+public class WebConfiguration {
+}
---
+++ src/main/java/by/fakeonliner/controller/FakeController.java
@@ -0,0 +1,7 @@
+package by.fakeonliner.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FakeController {
+}
