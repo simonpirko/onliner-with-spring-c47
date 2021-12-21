1123acec7bf64c3fc7a64078f954b79e72d2cb63
Dimon <dima2000@tut.by>
2021-12-21 12:34:31 +0300

    fix HibernateShopDao

--- src/main/java/by/fakeonliner/repository/hibernate/HibernateShopDao.java
+++ src/main/java/by/fakeonliner/repository/hibernate/HibernateShopDao.java
@@ -6,9 +6,12 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Repository
+@Transactional
 public class HibernateShopDao implements ShopDao {
 
     @Autowired
