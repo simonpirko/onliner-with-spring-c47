85007d2b5c5850b9f97703ab580364bc0f05c9dc
Dimon <dima2000@tut.by>
2021-12-25 23:52:07 +0300

    add UserDao

--- src/main/java/by/fakeonliner/controller/HomeController.java
+++ src/main/java/by/fakeonliner/controller/HomeController.java
@@ -1,7 +1,11 @@
 package by.fakeonliner.controller;
 
 
+import by.fakeonliner.dao.hibernate.HibernateUserDao;
+import by.fakeonliner.entity.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,8 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
+    @Autowired
+    private HibernateUserDao hibernateUserDao;
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("user", new User());
         return "home";
     }
 }
--- src/main/java/by/fakeonliner/dao/hibernate/HibernateUserDao.java
+++ src/main/java/by/fakeonliner/dao/hibernate/HibernateUserDao.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.NoResultException;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Repository
 @Transactional
+@Service
 public class HibernateUserDao implements UserDao {
 
     @Autowired
