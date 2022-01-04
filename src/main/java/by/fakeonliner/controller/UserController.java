f7e2015eaf3f1a8646794e70092bcef3e5aef086
Dimon <dima2000@tut.by>
2022-01-04 22:15:57 +0300

    add user controller and add user html

---
+++ src/main/java/by/fakeonliner/controller/UserController.java
@@ -0,0 +1,80 @@
+package by.fakeonliner.controller;
+
+
+import by.fakeonliner.dao.hibernate.HibernateUserDao;
+import by.fakeonliner.entity.user.RoleUser;
+import by.fakeonliner.entity.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private HibernateUserDao hibernateUserDao;
+
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("newUser", new User());
+        return "/user/registration";
+    }
+
+    @PostMapping("/registration")
+    public String registration(@ModelAttribute("newUser") User user, Model model, BindingResult result) {
+        try {
+            if (result.hasErrors()) {
+                return "/user/registration";
+            }
+
+            if (hibernateUserDao.existByEmail(user.getEmail())) {
+//                model.addAttribute("message", "Email already exist");
+                model.addAttribute("message", true);
+                return "/user/registration";
+            } else {
+                user.setRoleUser(RoleUser.USER);
+                hibernateUserDao.save(user);
+                return "redirect:/user/authorization";
+            }
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "Error: " + e.getMessage());
+        }
+        return "/user/registration";
+    }
+
+    @GetMapping("/authorization")
+    public String authorization(Model model) {
+        model.addAttribute("authUser", new User());
+        return "/user/authorization";
+    }
+
+    @PostMapping("/authorization")
+    public String authorization(@ModelAttribute("authUser") User user, Model model, HttpSession httpSession) {
+        User byUser = hibernateUserDao.findByEmail(user.getEmail());
+        try {
+            if (byUser != null) {
+                if (byUser.getPassword().equals(user.getPassword())) {
+                    httpSession.setAttribute("authUser", byUser);
+                    return "redirect:/";
+                } else {
+                    model.addAttribute("messagePassword", true);
+                }
+            } else {
+                model.addAttribute("messageUser", true);
+            }
+
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "Error: " + e.getMessage());
+        }
+        return "/user/authorization";
+    }
+
+}
