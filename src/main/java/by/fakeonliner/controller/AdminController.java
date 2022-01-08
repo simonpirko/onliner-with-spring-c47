8dc2a783ac2a0bdaa079188b780a4b244bc69337
igor1027 <prudnikow123@mail.ru>
2022-01-09 01:01:01 +0300

    ONL-22.1 update admin controller + add new Html

--- src/main/java/by/fakeonliner/controller/AdminController.java
+++ src/main/java/by/fakeonliner/controller/AdminController.java
@@ -2,9 +2,13 @@ package by.fakeonliner.controller;
 
 import by.fakeonliner.dao.hibernate.HibernateProductDao;
 import by.fakeonliner.dao.hibernate.HibernateUserDao;
+import by.fakeonliner.entity.product.Category;
 import by.fakeonliner.entity.product.Product;
 import by.fakeonliner.entity.user.RoleUser;
 import by.fakeonliner.entity.user.User;
+import by.fakeonliner.service.CategoryService;
+import by.fakeonliner.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,13 +16,26 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
     private HibernateUserDao hibernateUserDao;
+
+    @Autowired
     private HibernateProductDao hibernateProductDao;
+
+    private final ProductService productService;
+
+    private final CategoryService categoryService;
+
+    public AdminController(ProductService productService, CategoryService categoryService) {
+        this.productService = productService;
+        this.categoryService = categoryService;
+    }
 
     @GetMapping("/authorizationAdmin")
     public String authorization(Model model) {
@@ -34,7 +51,7 @@ public class AdminController {
                 return "/admin/authorizationAdmin";
             }
             if(byUser != null){
-                if(user.getRoleUser() == RoleUser.ADMIN){
+                if(byUser.getRoleUser().equals(RoleUser.ADMIN)){
                     if (byUser.getPassword().equals(user.getPassword())){
                         httpSession.setAttribute("admin", byUser);
                         return "redirect:/admin/profileAdmin";
@@ -55,9 +72,9 @@ public class AdminController {
     }
 
     @GetMapping("/profileAdmin")
-    public String profile(@RequestParam("admin") long id, Model model) {
-        User user = hibernateUserDao.findById(id);
-        model.addAttribute("admin", user);
+    public String profile(User user, Model model) {
+        User byUser = hibernateUserDao.findById(user.getId());
+        model.addAttribute("adminProf", byUser);
         return "/admin/profileAdmin";
     }
 
@@ -83,9 +100,14 @@ public class AdminController {
     }
 
     @GetMapping("/deleteProduct")
-    public String deletedProduct(@RequestParam("admin") long id, Model model){
-        User user = hibernateUserDao.findById(id);
-        model.addAttribute("admin", user);
+    public String deletedProduct(User user, Model model){
+        User byUser = hibernateUserDao.findById(user.getId());
+        model.addAttribute("admin", byUser);
+
+        List<Category> categoryList = categoryService.getCategory();
+        List<Product> productList = productService.getAllProducts();
+        model.addAttribute("categoryList", categoryList);
+        model.addAttribute("productList", productList);
         return "/admin/deleteproduct";
     }
 
