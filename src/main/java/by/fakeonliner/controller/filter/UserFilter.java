ff82f7677d34a85a31621482af6d2aa418fbc32c
Dimon <dima2000@tut.by>
2022-01-17 01:01:33 +0300

    small fix button basket

--- src/main/java/by/fakeonliner/controller/filter/UserFilter.java
+++ src/main/java/by/fakeonliner/controller/filter/UserFilter.java
@@ -2,9 +2,12 @@ package by.fakeonliner.controller.filter;
 
 
 import by.fakeonliner.entity.user.User;
+import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -13,14 +16,24 @@ import java.io.IOException;
 
 
 @WebFilter("/admin")
-public class UserFilter extends HttpFilter {
+public class UserFilter extends GenericFilterBean {
 
-    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        User user = (User) req.getSession().getAttribute("authUser");
-        if (user.getRoleUser().toString()=="ADMIN") {
-            chain.doFilter(req, res);
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        User user = (User) request.getAttribute("authUser");
+        if (user.getRoleUser().toString().equals("ADMIN")) {
+            chain.doFilter(request, response);
         } else {
-            res.sendRedirect("/authorization");
+//            response.sendRedirect("/authorization");
         }
     }
+
+//    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+//        User user = (User) req.getSession().getAttribute("authUser");
+//        if (user.getRoleUser().toString().equals("ADMIN")) {
+//            chain.doFilter(req, res);
+//        } else {
+//            res.sendRedirect("/authorization");
+//        }
+//    }
 }
